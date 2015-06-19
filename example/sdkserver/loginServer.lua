444426f9c54413d370305f1b47506c0e0aaf5553
sniperHW <huangweilook@21cn.com>
2015-06-19 17:36:34 +0800

    删除util.lua

--- example/sdkserver/loginServer.lua
+++ example/sdkserver/loginServer.lua
@@ -1,21 +1,12 @@
 local Router = require("lua.router")
 local cjson  = require("cjson")
-local Util = require("lua.util")
-
-local function parse(param)
-	local tmp1 = Util.SplitString(param,"&")
-	param = {}
-	for k,v in pairs(tmp1) do
-		local tmp2 = Util.SplitString(v,"=")
-		if #tmp2 == 2 then
-			param[tmp2[1]] = tmp2[2]
-		end
-	end
-	return param
-end
 
 Router.RegHandler("/loginServer",function (req,res,param)
-	param = parse(param)
+	local params = {}
+	for k, v in string.gmatch(param, "(%w+)=(%w+)") do
+  		params[k] = v
+	end	
+
 	local result = {
 		state = 0,
 		data = {
--- lua/router.lua
+++ lua/router.lua
@@ -1,38 +1,21 @@
-local Util = require("lua.util")
-
 local router = {
 	handler = {}
 }
 
-
---[[/getToken?
-sign=a2e5cb9674631820e7881f06b47adc0d
-&extension=&
-sid=sst1game803225ed07ef42448640b05006fcbb5f127783
-&appID=1
-&channelID=10
-]]--
-
 local function parse(req)
 	local method = req:GetMethod()
-	local tmp1 = Util.SplitString(req:GetUrl(),"?")
-	if #tmp1 == 0 then
-		return
+	local urlstr = req:GetUrl()
+	local url
+	for w in string.gmatch(urlstr, "/%a+") do
+		url = w
 	end
-	local url    = tmp1[1]
 	if method == "POST" then
 		return url,req:GetBody()
 	elseif method == "GET" then
 		local param = {}
-		if tmp1[2] then
-			tmp1 = Util.SplitString(tmp1[2],"&")
-			for k,v in pairs(tmp1) do
-				local tmp2 = Util.SplitString(v,"=")
-				if #tmp2 == 2 then
-					param[tmp2[1]] = tmp2[2]
-				end
-			end
-		end
+		for k, v in string.gmatch(urlstr, "(%w+)=(%w+)") do
+  			param[k] = v
+		end		
 		return url,param
 	else
 		return
